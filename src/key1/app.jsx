28ea8c96ecc1a9646a7826a43ca21b6d8f5c677d
zhangsong <zhangsong@guanghe.tv>
2019-01-07 19:58:43 +0800

    develop

--- src/key1/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Key1 extends React.Component{
-    render() {
-        return <h3>Key1</h3>
-    }
-}
-
-export default Key1
--- src/key2/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Key2 extends React.Component{
-    render() {
-        return <h3>Key2</h3>
-    }
-}
-
-export default Key2
